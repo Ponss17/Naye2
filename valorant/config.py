9bbdae387e7244e90a2be596c409c5e8abedc74a
Jean Castro <jeancastrogudiel@gmail.com>
2025-11-05 17:15:08 -0600

    Update config.py

--- valorant/config.py
+++ valorant/config.py
@@ -1,7 +1,11 @@
 import os
 
+# El nombre del jugador debe ser igual al que se usa en el juego.
+# El tg no contiene el hastag(#) solo el valor.
 NOMBRE = "Ponssloveless"
 TAG    = "8882"
 REGION = "na"
 
+# Aqui puedes definir tu api key de HenrikDev.
+# Se recomienda setearla como variable de entorno o utilizar un archivo .env.
 API_KEY = os.environ.get("API_KEY", "")
